626277fffcc4035c2629440b952405ac12ff679d
samridhi2003 <104695430+samridhi2003@users.noreply.github.com>
2023-05-12 16:17:15 +0530

    Updates

---
+++ precache-manifest.f2d9f06858436bad0b79414109c3704d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1ec3654288eb8912706",
+    "url": "/robofriends/static/css/main.84b11e80.chunk.css"
+  },
+  {
+    "revision": "e1ec3654288eb8912706",
+    "url": "/robofriends/static/js/main.e1ec3654.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/robofriends/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "2288987be6f91f473c99",
+    "url": "/robofriends/static/css/2.57c03eb8.chunk.css"
+  },
+  {
+    "revision": "2288987be6f91f473c99",
+    "url": "/robofriends/static/js/2.2288987b.chunk.js"
+  },
+  {
+    "revision": "0e09ba9c7502e90c21a0",
+    "url": "/robofriends/static/js/runtime~main.0e09ba9c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1934e57161ed15de2e850e204b81c3f6",
+    "url": "/robofriends/index.html"
+  }
+];
